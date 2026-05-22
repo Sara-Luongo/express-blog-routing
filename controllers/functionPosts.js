c0b56fc3f3d0bf5c483c4aba32e914ac0ba88dd1
Sara-Luongo <saraluongo202002@gmail.com>
2026-05-22 18:30:12 +0200

    inserita rotta e funzione show che mostra il singolo oggetto in base all'id (valore che gli passo io tramite la rotta) con vari controlli

--- controllers/functionPosts.js
+++ controllers/functionPosts.js
@@ -1,12 +1,48 @@
-import blogPosts from '../data/blogPosts.js';
+import posts from '../data/blogPosts.js';
 
 
 function index(request, response) {
-    response.json(blogPosts);
+    response.json(posts);
 }
 
+function show(request, response) {
+    const { id } = request.params;
+    const idReal = Number(id)
 
+    if (isNaN(idReal)) {
+        response.status(404)
+            .json({
+                errore: 'id non corretto',
+                results: null
+            });
+        return;
+    }
+    if (idReal <= 0) {
+        response.status(404)
+            .json({
+                errore: 'valore id negativo',
+                results: null
+            });
+        return;
+    }
+    const foundPost = posts.find(post => {
+        return post.id === idReal
+    });
+    if (foundPost === undefined) {
+        response.status(404)
+            .json({
+                errore: 'id inesistente',
+                results: null
+            });
+        return;
+    }
+    response.json({
+        error: null,
+        results: foundPost
+    });
+}
 
 export {
     index,
+    show,
 }
--- routes/postsRoutes.js
+++ routes/postsRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { index } from '../controllers/functionPosts.js';
+import { index, show } from '../controllers/functionPosts.js';
 
 //creo il mio router e lo esporto 
 const postsRouter = express.Router();
 
 //route index
 postsRouter.get('/', index);
+//route show che mi restituisce il singolo elemento del mio json tramite id parametro che gli passo io 
+postsRouter.get('/:id', show)
 
 export default postsRouter;
